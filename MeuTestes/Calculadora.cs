54e43c74ebc0942d858c6fd9450394b093a4aa4d
andrehenriquesouzacontato <155656799+andrehenriquesouzacontato@users.noreply.github.com>
2024-04-24 14:53:43 -0300

    Adicionar arquivos de projeto.

---
+++ MeuTestes/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeuTestes
+{
+    public class Calculadora
+    {
+        private List<string> listahistorico;
+
+        public Calculadora()
+        {
+            listahistorico = new List<string>();
+        }
+
+        public int somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+
+        public int subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+
+        public int multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+
+        public int dividir(int val1, int val2)
+        {
+            if (val2 == 0)
+                throw new DivideByZeroException("Não é possível dividir por zero.");
+
+            int res = val1 / val2;
+            listahistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            if (listahistorico.Count > 3)
+                listahistorico.RemoveRange(3, listahistorico.Count - 3);
+
+            return listahistorico;
+        }
+    }
+}
